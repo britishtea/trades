b9f8509981a35649878fdae1377916433bb80341
Paul Brickfeld <Jip@MacBook.local>
2011-06-29 19:54:15 +0200

    Added colors to the output
    Displays the price in green if the price rose, displays the price in red if the price sunk, displays in standard color if the price stays the same.

--- lib/trades.rb
+++ lib/trades.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'socket'
 require 'json'
+require 'colored'
 
 class Trades
   def initialize(args = [])
-    @socket    = TCPSocket.open("bitcoincharts.com", 27007)
+    @socket = TCPSocket.open("bitcoincharts.com", 27007)
     
     # Convert the exchange names to lowercase
     args.collect do |exchange|
@@ -13,8 +14,10 @@ class Trades
     
     #     thUSD          | 2011-06-27 23:00:55 | USD      | 17.05  | 0.5    |
     puts "Exchange       | Date and time       | Currency | Price  | Volume |"
-      
+    
     while data = @socket.gets do
+      last_prices = {}
+      
       if data.length > 0
         begin
           j = JSON.parse(data.strip.tr("\x00", '')) # Delete all \x00
@@ -27,8 +30,22 @@ class Trades
           line += Time.at(j['timestamp']).strftime("%Y-%m-%d %H:%M:%S").\
           ljust(20)
           line += "| " + j['currency'].ljust(9) + "| "
-          line += j['price'].round(2).to_s.ljust(7) + "| "
-          line += j['volume'].round(2).to_s.ljust(7) + "|"
+          
+          # Coloring: Compare the current price and the last price
+          current_price = j['price'].round(2)
+          last_price    = last_prices[j['symbol']]
+          
+          if current_price > last_price then
+            line += price.to_s.ljust(7).green
+          elsif current_price < last_price then
+            line += price.to_s.ljust(7).red
+          else
+            line += price.to_s.ljust(7)
+          end
+          
+          last_prices[j['symbol']] = current_price
+          
+          line += "| " + j['volume'].round(2).to_s.ljust(7) + "|"
     
           puts line
         end
