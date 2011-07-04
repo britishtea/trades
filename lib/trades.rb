ce8020b3d8bcf16db83f4459576389d8822627f3
Paul Brickfeld <Jip@MacBook.local>
2011-07-04 21:38:55 +0200

    Revert 01caef9eed77cd83a031d0fa7848c119cc5e9309^..HEAD

--- lib/trades.rb
+++ lib/trades.rb
@@ -4,20 +4,20 @@ require 'json'
 require 'colored'
 
 class Trades
+  attr_reader :markets, :last_prices
+  
   def initialize(args = [])
     begin
-      puts "Connecting to bitcoincharts.com..."
       @socket      = TCPSocket.open("bitcoincharts.com", 27007)
-      @markets     = []
+      @markets   = []
       @last_prices = {}
 
-      # Convert the market names to lowercase
-      # (CHANGE THIS TO CASE INSENSITIVE REGEX)
+      # Convert the exchange names to lowercase
       args.each do |x| @markets.push(x.downcase) end
       args.clear # no need to keep them in memory
 
-      #     thUSD          | 2011-06-27 23:00:55 | USD      | 17.05    | 0.5       |
-      puts "Exchange       | Date and time       | Currency | Price    | Volume    |"
+      #     thUSD          | 2011-06-27 23:00:55 | USD      | 17.05  | 0.5    |
+      puts "Exchange       | Date and time       | Currency | Price  | Volume |"
       
       self.loop
     rescue
@@ -25,26 +25,32 @@ class Trades
     end
   end
   
-  def loop
+  def loop(&block)
     while data = @socket.gets do
-      if data.length > 0 then 
+      if data.length > 0
         j = JSON.parse(data.strip.tr("\x00", '')) # Delete all \x00 weirdness
     
         if @markets.empty? or @markets.include?(j['symbol'].downcase) then
-          line  = j['symbol'].ljust(15) + "| " # market
-          line += Time.at(j['timestamp']).strftime("%Y-%m-%d %H:%M:%S") + " | "
-          line += j['currency'].ljust(9) + "| " # currency
+          line  = j['symbol'].ljust(15) + "| "
+          line += Time.at(j['timestamp']).strftime("%Y-%m-%d %H:%M:%S").\
+          ljust(20)
+          line += "| " + j['currency'].ljust(9) + "| "
           
-          # Price: display higher price in green / lower price in red
+          # Coloring: Compare the current price and the last price
           current_price = j['price'].round(2)
-          last_price    = @last_prices[j['symbol']].to_f # Its nil on startup
-          @last_prices[j['symbol']] = current_price # store for next comparison
+          last_price    = @last_prices[j['symbol']].to_f # Its 0 on startup
           
-          price = current_price.to_s.ljust(9)
-          price = price.green if current_price > last_price unless last_price.zero?
-          price = price.red if current_price < last_price
+          if current_price > last_price then
+            line += current_price.to_s.ljust(7).green
+          elsif current_price < last_price then
+            line += current_price.to_s.ljust(7).red
+          else
+            line += current_price.to_s.ljust(7)
+          end
           
-          line += price + "| " + j['volume'].round(2).to_s.ljust(9) + " |"
+          @last_prices[j['symbol']] = current_price
+          
+          line += "| " + j['volume'].round(2).to_s.ljust(7) + "|"
     
           puts line
         end
