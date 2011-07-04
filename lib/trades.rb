8a8f253c15edb90a90255158305839b3e299ed56
Paul Brickfeld <Jip@MacBook.local>
2011-07-04 21:18:31 +0200

    I don't remember

--- lib/trades.rb
+++ lib/trades.rb
@@ -9,24 +9,51 @@ class Trades
   def initialize(args = [])
     begin
       @socket      = TCPSocket.open("bitcoincharts.com", 27007)
-      @markets     = []
+      @markets   = []
       @last_prices = {}
 
-      # Convert the market names to lowercase
-      # (CHANGE THIS TO CASE INSENSITIVE REGEX)
+      # Convert the exchange names to lowercase
       args.each do |x| @markets.push(x.downcase) end
       args.clear # no need to keep them in memory
+
+      #     thUSD          | 2011-06-27 23:00:55 | USD      | 17.05  | 0.5    |
+      puts "Exchange       | Date and time       | Currency | Price  | Volume |"
+      
+      self.loop
     rescue
       puts "Error: #{$!}"
     end
   end
   
-  def run(&block)
+  def loop(&block)
     while data = @socket.gets do
-      if data.length > 0 then 
+      if data.length > 0
         j = JSON.parse(data.strip.tr("\x00", '')) # Delete all \x00 weirdness
-        
-        yield j['symbol'], j['timestamp'], j['currency'], j['price'], j['volume']
+    
+        if @markets.empty? or @markets.include?(j['symbol'].downcase) then
+          line  = j['symbol'].ljust(15) + "| "
+          line += Time.at(j['timestamp']).strftime("%Y-%m-%d %H:%M:%S").\
+          ljust(20)
+          line += "| " + j['currency'].ljust(9) + "| "
+          
+          # Coloring: Compare the current price and the last price
+          current_price = j['price'].round(2)
+          last_price    = @last_prices[j['symbol']].to_f # Its 0 on startup
+          
+          if current_price > last_price then
+            line += current_price.to_s.ljust(7).green
+          elsif current_price < last_price then
+            line += current_price.to_s.ljust(7).red
+          else
+            line += current_price.to_s.ljust(7)
+          end
+          
+          @last_prices[j['symbol']] = current_price
+          
+          line += "| " + j['volume'].round(2).to_s.ljust(7) + "|"
+    
+          puts line
+        end
       end
     end
   end
